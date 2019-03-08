94dbb13b2d767371f4744ec4bb81bdf8c11c5cbd
Eric Mauldin <emauldin84@gmail.com>
2019-03-08 09:04:52 -0500

    adds div and data-target attribute to each printed name

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,17 @@ const leftColumn = document.querySelector('[data-left-output]');
 
 // function appendNamesToLeft(characters) {
 characters.forEach(function(element) {
-    console.log(element.name);
+    const charDiv = document.createElement('div');
+    const nameIden = document.createAttribute("data-target");
+    
+    
     leftColumn.append(charDiv);
-    charDiv.append(element.name + ', ')
+    charDiv.append(element.name);
+
+    charDiv.setAttributeNode(nameIden);
+
+    // console.log(element.name);
+    console.log(charDiv);
 });
 
 // };
